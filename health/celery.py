e51be95d7435071ab8f470369ef5347623a24f18
yahia <yahiaeltaralm@gmail.com>
2024-12-25 10:45:04 +0200

    debug error

--- health/celery.py
+++ health/celery.py
@@ -14,7 +14,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.conf.beat_schedule = {
     'send-scheduled-email': {
-        'task': 'your_app.tasks.send_scheduled_email',
+        'task': 'api.tasks.send_scheduled_email',
         'schedule': crontab(minute=0, hour=9),  # This will run daily at 9:00 AM
         'args': ("Scheduled Email", "This is a scheduled test email.", ['recipient@example.com']),
     },
